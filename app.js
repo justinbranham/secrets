f5f1a027860ce8efc6a9caf08570c34bd9f96c4f
justinbranham <54411431+justinbranham@users.noreply.github.com>
2020-06-12 16:21:33 -0400

    Update connection string to use MongoDB Atlas.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-justin:Test123@cluster0-kq813.mongodb.net/userDB", {useNewUrlParser: true});
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
